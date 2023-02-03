ef737604dc87473d511aec6040741aecda2c8912
thanhvdt <tthanhvuong11@gmail.com>
2023-02-03 22:19:03 +0700

    Frontend: Add api test

--- frontend/src/test/APITest/searchapi_vn.test.js
+++ frontend/src/test/APITest/searchapi_vn.test.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 test("search API returns expected results", async () => {
   const searchTerm = "abequose";
-  const response = await axios.get("http://localhost:5000/search_bar", {
+  const response = await axios.get("https://meddict-vinuni.com/search_bar", {
     params: { word: searchTerm, lang: "vn" },
   });
   const results = response.data[0]["en"];
