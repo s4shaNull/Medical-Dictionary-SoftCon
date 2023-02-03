a3d0f18a25e5a7ac686a3187345aee33a62d5b54
thanhvdt <tthanhvuong11@gmail.com>
2023-02-03 23:54:56 +0700

    Frontend: Fix API Test

--- frontend/src/test/APITest/searchapi_en.test.js
+++ frontend/src/test/APITest/searchapi_en.test.js
@@ -2,9 +2,12 @@ const axios = require("axios");
 
 test("search API returns expected results", async () => {
   const searchTerm = "abequose";
-  const response = await axios.get("https://meddict-vinuni.com/search_bar", {
-    params: { word: searchTerm, lang: "en" },
-  });
+  const response = await axios.get(
+    "http://meddict-vinuni.com:5000/search_bar",
+    {
+      params: { word: searchTerm, lang: "en" },
+    }
+  );
   const results = response.data[0]["vn"];
 
   expect(results).toContain(searchTerm);
--- frontend/src/test/APITest/searchapi_vn.test.js
+++ frontend/src/test/APITest/searchapi_vn.test.js
@@ -2,9 +2,12 @@ const axios = require("axios");
 
 test("search API returns expected results", async () => {
   const searchTerm = "abequose";
-  const response = await axios.get("https://meddict-vinuni.com/search_bar", {
-    params: { word: searchTerm, lang: "vn" },
-  });
+  const response = await axios.get(
+    "http://meddict-vinuni.com:5000/search_bar",
+    {
+      params: { word: searchTerm, lang: "vn" },
+    }
+  );
   const results = response.data[0]["en"];
 
   expect(results).toContain(searchTerm);
