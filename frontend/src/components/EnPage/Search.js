d7ba56a241c65dd62da593bb33bda77a5a4d121a
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2023-02-14 04:18:06 +0700

    new search bar

--- frontend/src/components/EnPage/Search.js
+++ frontend/src/components/EnPage/Search.js
@@ -4,63 +4,91 @@ import { ReactSearchAutocomplete } from "react-search-autocomplete";
 import "../style/base.css";
 import "../style/main.css";
 import "../style/responsive.css";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 function Search(props) {
-  const [items, setItems] = useState([]);
+
+  // const [items, setItems] = useState([]);
+  // const fromEng = props.fromEng;
+  // // note: the id field is mandatory
+
+  // const handleOnSearch = (string) => {
+  //   if (string != "") {
+  //     const loadData = async () => {
+  //       // THE ADDR BELOW MUST BE THE DOMAIN NAME OF THE CLOUD SERVER WHEN DEPLOYED!
+  //       const response = await axios.get("http://localhost:5000/search_bar", {
+  //         params: { word: string, lang: fromEng ? "en" : "vn" },
+  //       });
+  //       setItems(response.data);
+  //     };
+  //     loadData();
+  //   }
+  // };
+
+  // const handleOnHover = (result) => {
+  //   // console.log(result);
+  // };
+
+  // const handleOnSelect = (item) => {
+  //   // THE ADDR BELOW MUST BE THE DOMAIN NAME SERVER OF THE CLOUD SERVER WHEN DEPLOYED
+  //   axios
+  //     .get("http://localhost:5000/audio", {
+  //       params: { en_word: item.en, vi_word: item.vn },
+  //     })
+  //     .then((response) => { });
+
+  //   props.setShowResult(!props.showResult);
+  //   props.setResult({
+  //     en: `${item.en}`,
+  //     vn: `${item.vn}`,
+  //     type: `${item.word_type}`,
+  //     type_vn: `${item.word_type_vn}`,
+  //   });
+  // };
+
+  // const handleOnFocus = () => {
+  //   // console.log("Focused");
+  // };
+
+  // const handleOnClear = () => {
+  //   // console.log("Cleared");
+  // };
+
+  // const formatResult = (item) => {
+  //   return (
+  //     <>
+  //       <span style={{}}>
+  //         {fromEng ? item.en : item.vn} ({item.word_type})
+  //       </span>
+  //     </>
+  //   );
+  // };
+  const [options, setOptions] = useState([]);
+  const [value, setValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
   const fromEng = props.fromEng;
-  // note: the id field is mandatory
 
-  const handleOnSearch = (string) => {
-    if (string != "") {
-      const loadData = async () => {
-        // THE ADDR BELOW MUST BE THE DOMAIN NAME OF THE CLOUD SERVER WHEN DEPLOYED!
+  useEffect(() => {
+    const getResults = async () => {
+      try {
         const response = await axios.get("http://localhost:5000/search_bar", {
-          params: { word: string, lang: fromEng ? "en" : "vn" },
+          params: { word: inputValue, lang: fromEng ? "en" : "vn" },
         });
-        setItems(response.data);
-      };
-      loadData();
-    }
-  };
+        var myOptions = []
+        for (var i = 0; i < response.data.length; i++) {
+          myOptions.push(response.data[i].vn)
+        }
+        setOptions(myOptions)
 
-  const handleOnHover = (result) => {
-    // console.log(result);
-  };
 
-  const handleOnSelect = (item) => {
-    // THE ADDR BELOW MUST BE THE DOMAIN NAME SERVER OF THE CLOUD SERVER WHEN DEPLOYED
-    axios
-      .get("http://localhost:5000/audio", {
-        params: { en_word: item.en, vi_word: item.vn },
-      })
-      .then((response) => {});
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    props.setShowResult(!props.showResult);
-    props.setResult({
-      en: `${item.en}`,
-      vn: `${item.vn}`,
-      type: `${item.word_type}`,
-      type_vn: `${item.word_type_vn}`,
-    });
-  };
-
-  const handleOnFocus = () => {
-    // console.log("Focused");
-  };
-
-  const handleOnClear = () => {
-    // console.log("Cleared");
-  };
-
-  const formatResult = (item) => {
-    return (
-      <>
-        <span style={{}}>
-          {fromEng ? item.en : item.vn} ({item.word_type})
-        </span>
-      </>
-    );
-  };
+    getResults();
+  }, [value, inputValue]);
 
   return (
     <div className="header__search">
@@ -73,7 +101,7 @@ function Search(props) {
           props.setShowSearchMobile(!props.showSearchMobile);
         }}
       >
-        <ReactSearchAutocomplete
+        {/* <ReactSearchAutocomplete
           items={items}
           onSearch={handleOnSearch}
           onHover={handleOnHover}
@@ -88,6 +116,24 @@ function Search(props) {
           styling={{
             lineColor: "#c92127",
           }}
+        /> */}
+        <Autocomplete
+          value={value}
+          style={{ width: 500 }}
+          freeSolo
+          autoComplete
+          autoHighlight
+          options={options}
+          onChange={(event, newValue) => {
+            setOptions(newValue ? [newValue, ...options] : options);
+            setValue(newValue);
+          }}
+          onInputChange={(event, newInputValue) => {
+            setInputValue(newInputValue);
+          }}
+          renderInput={(params) => (
+            <TextField {...params} label="" variant="outlined" />
+          )}
         />
       </div>
     </div>
